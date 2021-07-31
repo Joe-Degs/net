ee672571d74d0ee2174334687fafee53b1e74a46
Joe-Degs <jpedegs8990@gmail.com>
2021-07-31 01:59:01 +0100

    add socket errors exception

--- net/errors.py
+++ net/errors.py
@@ -2,9 +2,13 @@ class Error(Exception):
     def __init__(self, message):
         super().__init__(message)
 
+class SocketError(Error):
+    def __init__(self, reason: str):
+        super().__init__(f'socket: {reason}')
+
 class AddressError(Error):
     def __init__(self, expression, reason):
-        super().__init__(f'{expression}: {reason}')
+        super().__init__(f'address: {expression} - {reason}')
 
 class UnknownNetworkError(Error):
     def __init__(self, network: str):
